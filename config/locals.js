bad21f4925df7bb84318b44bbbbc169aefee5935
Paterna <roberto.paterna@gmail.com>
2016-11-24 09:42:33 +0100

    SSL config (default disabled)

---
+++ config/locals.js
@@ -0,0 +1,10 @@
+// var fs = require('fs');
+
+// module.exports = {
+
+//     ssl : {
+//         key: fs.readFileSync('/home/roberto/donkus/donkus.key'),
+//        cert: fs.readFileSync('/home/roberto/donkus/donkus.crt')
+//     }
+
+// };
